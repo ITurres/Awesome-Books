cd0217c7534dfac6a24cd3c24b383fccb420cbca
iturres <emanueliturres.co@gmail.com>
2023-05-30 13:57:03 -0300

    Modify 'user-controller-events' -add- and -remove- events

--- src/assets/js/user-controller/user-controller-events.js
+++ src/assets/js/user-controller/user-controller-events.js
@@ -6,21 +6,20 @@ if (window.localStorage.length > 0) {
   updateBooksListOnDOM('allBooks');
 }
 
-document
-  .querySelector('[data-add-note-btn]')
-  .addEventListener('click', (event) => {
-    const bookTitle = document.querySelector('input[name="title"]').value;
-    const bookAuthor = document.querySelector('input[name="author"]').value;
-    if (!bookTitle || !bookAuthor) {
-      event.preventDefault();
-      return;
-    }
-    storeNewBookData(bookTitle, bookAuthor);
-    updateBooksListOnDOM('lastBook');
-  });
+document.querySelector('[data-add-note-btn]').addEventListener('click', () => {
+  const bookTitle = document.querySelector('input[name="title"]').value;
+  const bookAuthor = document.querySelector('input[name="author"]').value;
+  if (!bookTitle || !bookAuthor) {
+    return;
+  }
+  storeNewBookData(bookTitle, bookAuthor);
+  window.location.reload();
+  updateBooksListOnDOM('lastBook');
+});
 
 document.querySelectorAll('[data-remove-book-btn]').forEach((button) => {
   button.addEventListener('click', () => {
     removeBook(Number(button.id));
+    window.location.reload();
   });
 });
