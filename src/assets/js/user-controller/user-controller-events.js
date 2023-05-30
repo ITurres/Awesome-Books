7c4f276526c8952a268c994f6e98057726326277
iturres <emanueliturres.co@gmail.com>
2023-05-30 18:41:47 -0300

    Modify 'user-controller-events' to make use of the new Class 'bookList'

--- src/assets/js/user-controller/user-controller-events.js
+++ src/assets/js/user-controller/user-controller-events.js
@@ -1,25 +1,28 @@
 import updateBooksListOnDOM from '../markup-injectors/inject-update-books-list-on-dom.js';
-import storeNewBookData from './books-data-localStorage.js';
-import removeBook from './remove-book.js';
+import BookList from './book-list-class.js';
 
 if (window.localStorage.length > 0) {
   updateBooksListOnDOM('allBooks');
 }
 
 document.querySelector('[data-add-note-btn]').addEventListener('click', () => {
-  const bookTitle = document.querySelector('input[name="title"]').value;
-  const bookAuthor = document.querySelector('input[name="author"]').value;
-  if (!bookTitle || !bookAuthor) {
+  const bookTitle = document.querySelector('input[name="title"]');
+  const bookAuthor = document.querySelector('input[name="author"]');
+  if (!bookTitle.value || !bookAuthor.value) {
     return;
   }
-  storeNewBookData(bookTitle, bookAuthor);
+  const newBookList = new BookList();
+  newBookList.addNewBook(bookTitle.value, bookAuthor.value);
+  bookTitle.value = ''; // vendor for firefox
+  bookAuthor.value = ''; // vendor for firefox
   window.location.reload();
   updateBooksListOnDOM('lastBook');
 });
 
 document.querySelectorAll('[data-remove-book-btn]').forEach((button) => {
   button.addEventListener('click', () => {
-    removeBook(Number(button.id));
+    const newBookList = new BookList();
+    newBookList.removeBook(button.id);
     window.location.reload();
   });
 });
