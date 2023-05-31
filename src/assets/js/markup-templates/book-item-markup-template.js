fd7cdc1901969739f4e3ba7a9926a17750854e08
Vishal <vismiss.30@gmail.com>
2023-05-31 21:39:08 +0530

    Replace 'span' to 'td'

--- src/assets/js/markup-templates/book-item-markup-template.js
+++ src/assets/js/markup-templates/book-item-markup-template.js
@@ -1,9 +1,11 @@
 function createBookItemMarkupTemplate(book) {
-  return `<div class="book-item">
-    <span>${book.title}</span><br /><span>${book.author}</span><br />
-    <button id="${book.id}" type="button" data-remove-book-btn>Remove</button>
-    <hr />
-    </div>`;
+  return `<tr class="book-item">
+     <td>${book.title}</td>
+     <td>${book.author}</td>
+     <td>
+      <button id="${book.id}" type="button" data-remove-book-btn>Remove</button>
+     </td>
+    </tr>`;
 }
 
 export default createBookItemMarkupTemplate;
