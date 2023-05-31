84d553082a3f7b062fb07583236c98b7bc72f157
iturres <emanueliturres.co@gmail.com>
2023-05-31 14:49:17 -0300

    Add Bootstrap Classes to 'book item -button-' on markup-template

--- src/assets/js/markup-templates/book-item-markup-template.js
+++ src/assets/js/markup-templates/book-item-markup-template.js
@@ -1,10 +1,12 @@
 function createBookItemMarkupTemplate(book) {
-  return `<tr class="book-item d-flex-justify-content-center align-items-center">
-     <td>${book.title}</td>
-     <td>${book.author}</td>
-     <td>
-      <button id="${book.id}" class="ml-auto" type="button" data-remove-book-btn>Remove</button>
-     </td>
+  return `<tr class="book-item">
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td class="d-flex justify-content-end">
+        <button id="${book.id}" type="button"
+           data-remove-book-btn>Remove
+        </button>
+      </td>
     </tr>`;
 }
 
