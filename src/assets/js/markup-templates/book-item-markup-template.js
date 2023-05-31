725b44c5978fc0aea6cb851f8e26d9c3131cb4d4
Vishal <vismiss.30@gmail.com>
2023-05-31 22:22:05 +0530

    Add some bootstrap class to button and 'tr'

--- src/assets/js/markup-templates/book-item-markup-template.js
+++ src/assets/js/markup-templates/book-item-markup-template.js
@@ -1,9 +1,9 @@
 function createBookItemMarkupTemplate(book) {
-  return `<tr class="book-item">
+  return `<tr class="book-item d-flex-justify-content-center align-items-center">
      <td>${book.title}</td>
      <td>${book.author}</td>
      <td>
-      <button id="${book.id}" type="button" data-remove-book-btn>Remove</button>
+      <button id="${book.id}" class="ml-auto" type="button" data-remove-book-btn>Remove</button>
      </td>
     </tr>`;
 }
